c28b9eecc92bdf542abc414838de3840241bdb9e
youngfreezy <Fareez.Ahmed@gmail.com>
2016-07-09 17:04:10 +0600

    parent level component has the state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyDaLyBywgTR9PSAKSmbTL68Pxg8ll89dE0';
 
-const App = () => {
-  return (
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.setState({videos});
+    });
+  }
+
+  render () {
+  	return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos}/>
     </div>
-  )
+  	)
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector(".container"))
